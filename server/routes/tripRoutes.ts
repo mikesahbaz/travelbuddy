1d73440619b61b5e969837154405759cf9882c93
Giovanni Gonzales <therealgiog@gmail.com>
2023-05-06 13:41:38 -0400

    corrected tipRoutes

--- server/routes/tripRoutes.ts
+++ server/routes/tripRoutes.ts
@@ -4,8 +4,8 @@ const router = express.Router();
 import { createTrip, getAllTripsByUserId, getTripByTripId, getAllUsersByTripId, deleteTrip} from '../controllers/tripController';
 
 router.post('/create', createTrip);
-router.get('/:userId', getAllTripsByUserId);
-router.get('/:tripId', getTripByTripId);
+router.get('/tripsbyuser/:userId', getAllTripsByUserId);
+router.get('/tripsbytrip/:tripId', getTripByTripId);
 router.get('/users/:tripId', getAllUsersByTripId);
 router.delete('/delete/:tripId', deleteTrip);
 
