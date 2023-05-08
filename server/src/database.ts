0d0972c2e1d89e543bdd9dc3fe77f703a860d033
Mike Sahbaz <rsmikesahbaz@hotmail.com>
2023-05-08 13:15:28 -0600

    Added log statement to DB connection and first commit

--- server/src/database.ts
+++ server/src/database.ts
@@ -4,6 +4,7 @@ import { config } from './config/config';
 async function connectToDatabase () {
   try {
     await mongoose.connect(config.mongo.url, { retryWrites: true, w: 'majority'});
+    console.log('Succesfully connected to the DB');
   } catch (err) {
     console.log('Error connecting to the database: ', err);
     throw err;
