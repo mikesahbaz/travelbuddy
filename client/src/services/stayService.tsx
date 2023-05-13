a1905c09d227438df763e517f93e5696e43fb917
Mike Sahbaz <rsmikesahbaz@hotmail.com>
2023-05-13 08:40:59 -0600

    merging stayService

--- client/src/services/stayService.tsx
+++ client/src/services/stayService.tsx
@@ -3,11 +3,7 @@ import axios from "axios";
 
 export const toggleFavoriteStay = async (tripId: string, stayFormData: any) => {
   try {
-<<<<<<< HEAD
-    const response = await axios.put(`http://localhost:3001/stays/${tripId}/favorite`, propertyId);
-=======
     const response = await axios.put(`http://localhost:3001/stays/${tripId}/favorite`, stayFormData);
->>>>>>> new-mike-b
     return response.data;
   } catch (error) {
     console.error('Error in toggling favorite Stay', error);
